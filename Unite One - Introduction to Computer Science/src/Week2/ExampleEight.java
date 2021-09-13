0d904c18b8a3187041a579cbab698e3cf4368afc
Rod Khajehnoori <rkhajehnoori@bayviewglen.ca>
2021-09-13 14:31:01 -0400

    Extension of ++ and -- and extension on data types

---
+++ Unite One - Introduction to Computer Science/src/Week2/ExampleEight.java
@@ -0,0 +1,30 @@
+package Week2;
+/**
+ * Type Casting
+ */
+
+public class ExampleEight {
+    public static void main(String[] args) {
+        final int NUM_MARKS = 5;
+
+        int markOne = 78, markTwo= 87, markThree = 64, markFour = 95, markFive = 84;
+
+        double average = (double)(markOne + markTwo + markThree + markFour + markFive) /NUM_MARKS;
+
+
+        System.out.println("The average is: "+ average);
+
+        double y = 3.7;
+        int x = (int)y;
+
+        double z = (double)7;
+        //int --> double //widening conversion
+        //double --> int //narrowing conversion 
+
+        //boolean x = (int)true;
+
+
+        
+    }
+    
+}
---
+++ Unite One - Introduction to Computer Science/src/Week2/ExampleSeven.java
@@ -0,0 +1,24 @@
+package Week2;
+
+/**
+ * final modifier - constant 
+ */
+
+public class ExampleSeven {
+    public static void main(String[] args) {
+        final int x = 7;
+      //  x=0; cannot re assign a value to a final variable
+
+      final int y;
+      y = 8;
+     // y++; cannot modify the variable once it is set if it is constant
+
+
+     final int NUM_STUDENTS =  18; //naming convention for constants (final variable) is UPPER_CASE with_ for space
+
+     
+
+        
+    }
+    
+}
